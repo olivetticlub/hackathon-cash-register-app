72251d7a1f786301031a6376bb129f4a8b245449
Davide Bontempelli <davide.bonte@gmail.com>
2019-10-05 14:05:29 +0200

    add initial merchant creation

--- app/src/main/java/it/tim/innovation/jolmilano/cr40devapp/Main2Activity.kt
+++ app/src/main/java/it/tim/innovation/jolmilano/cr40devapp/Main2Activity.kt
@@ -8,10 +8,6 @@ import kotlinx.android.synthetic.main.activity_main2.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Body
-import retrofit2.http.POST
 
 class Main2Activity : AppCompatActivity() {
 
@@ -47,7 +43,14 @@ class Main2Activity : AppCompatActivity() {
 
 
         create_merchant.setOnClickListener {
-            olivettiClubBackend().createMerchant(MerchantCreationRequest("danielefogna", "123123123", "31.43.13", "via del cazzettino"))
+            service.createMerchant(
+                MerchantCreationRequest(
+                    "danielefogna",
+                    "123123123",
+                    "31.43.13",
+                    "via del cazzettino"
+                )
+            )
                 .enqueue(object :
                     Callback<MerchantCreationResponse> {
                     override fun onFailure(call: Call<MerchantCreationResponse>, t: Throwable) {
@@ -65,33 +68,5 @@ class Main2Activity : AppCompatActivity() {
         }
     }
 
-    fun olivettiClubBackend(): OlivettiClubBackendApi {
-        val olivettiClubBaseUrl = "http://olivetticlub.dallagi.dev:5000"
-
-        val retrofit = Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl(olivettiClubBaseUrl)
-            .build()
-
-        return retrofit.create(OlivettiClubBackendApi::class.java)
-    }
 
 }
-
-interface OlivettiClubBackendApi {
-
-    @POST("coupons")
-    fun createCoupon(@Body body: CouponCreationRequest): Call<CouponCreationResponse>
-
-    @POST("merchants")
-    fun createMerchant(@Body body: MerchantCreationRequest): Call<MerchantCreationResponse>
-}
-
-
-data class CouponCreationRequest(val merchant: String, val description: String, val count: Int)
-data class CouponCreationResponse(val merchant: String, val description: String, val count: Int)
-
-
-data class MerchantCreationRequest(val name: String, val vatNumber: String, val ateco: String, val address:String)
-data class MerchantCreationResponse(val name: String, val vatNumber: String, val ateco: String, val address:String, val deals: List<Deal>)
-data class Deal(val merchant:String, val description: String, val consumedCoupon: Int, val generatedCoupon:Int)
--- app/src/main/java/it/tim/innovation/jolmilano/cr40devapp/OlivettiClubBackendServiceFactory.kt
+++ app/src/main/java/it/tim/innovation/jolmilano/cr40devapp/OlivettiClubBackendServiceFactory.kt
@@ -20,14 +20,43 @@ class OlivettiClubBackendServiceFactory {
             return retrofit.create(OlivettiClubBackendApi::class.java)
         }
     }
-    
+
 }
 
 interface OlivettiClubBackendApi {
 
     @POST("coupons")
     fun createCoupon(@Body body: CouponCreationRequest): Call<CouponCreationResponse>
+
+
+    @POST("merchants")
+    fun createMerchant(@Body body: MerchantCreationRequest): Call<MerchantCreationResponse>
 }
+
 
 data class CouponCreationRequest(val merchant: String, val description: String, val count: Int)
 data class CouponCreationResponse(val merchant: String, val description: String, val count: Int)
+
+
+data class MerchantCreationRequest(
+    val name: String,
+    val vatNumber: String,
+    val ateco: String,
+    val address: String
+)
+
+data class MerchantCreationResponse(
+    val name: String,
+    val vatNumber: String,
+    val ateco: String,
+    val address: String,
+    val deals: List<Deal>
+)
+
+data class Deal(
+    val merchant: String,
+    val description: String,
+    val generated_coupons_count: String,
+    val consumed_coupons_count: String,
+    val id: Int
+)
